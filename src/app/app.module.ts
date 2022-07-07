c8d15959f5a14d556040015aeb7e43b010ab630b
FathimaZahrah <zahrahshafik13@gmail.com>
2022-07-07 16:19:17 +0530

    Search Places added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TravelregistrationComponent } from './travelregistration/travelregistration.component';
+import { SearchplacesComponent } from './searchplaces/searchplaces.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TravelregistrationComponent
+    TravelregistrationComponent,
+    SearchplacesComponent
   ],
   imports: [
     BrowserModule,
